091223780b8b14bf9316b69aee172acef33a2804
wangyong <whuwangyong@gmail.com>
2016-06-19 21:14:18 +0800

    a update

--- favorite_packages/install.sh
+++ favorite_packages/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install shutter guake axel sublime-text
-# apt-get install okular kdelibs5-plugins kdelibs5-data oxygen-icon-theme # pdf reader
+# apt-get install okular kdelibs5-plugins kdelibs5-data oxygen-icon-theme # a pdf reader from kde
 
 
